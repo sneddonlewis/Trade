f5287c1836ffb8003fcb7a3cd484b172b4fa7487
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2024-09-22 13:13:35 +0100

    basic ws server with counter

--- Trade.Rest/Program.cs
+++ Trade.Rest/Program.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseWebSockets();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,4 +28,31 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Map("/order_book", async ctx =>
+{
+    if (!ctx.WebSockets.IsWebSocketRequest)
+    {
+        ctx.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+
+    int counter = 1;
+    var ws = await ctx.WebSockets.AcceptWebSocketAsync();
+
+    while (true)
+    {
+        if (ws.State is WebSocketState.Closed or WebSocketState.Aborted)
+        {
+            return;
+        }
+        
+        string message = $"Counter {counter++}";
+        var bytes = Encoding.UTF8.GetBytes(message);
+        var arrSeg = new ArraySegment<byte>(bytes, 0, bytes.Length);
+        await ws.SendAsync(arrSeg, WebSocketMessageType.Text, true, CancellationToken.None);
+        
+        Thread.Sleep(1000);
+    }
+
+});
+
+await app.RunAsync();
